615283d50190f10f7dc7b4477b89bae37f7edcb1
SeanMcDonnell83 <sean.mcdonnell1983@gmail.com>
2026-02-19 02:52:13 +0000

    Vite added to client

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,9 +2,20 @@ import {defineConfig} from 'astro/config'
 import sanity from '@sanity/astro'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
-  integrations: [sanity(), react()],
+  integrations: [
+    sanity({
+      projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
+      dataset: process.env.PUBLIC_SANITY_DATASET,
+      apiVersion: process.env.PUBLIC_SANITY_API_VERSION || '2025-02-01',
+      useCdn: true,
+    }),
+    react(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
